b4929f118c86de3a77a2d55265b7372518f5cb4f
rafaellimati <rafaelprofissionalti@gmail.com>
2015-08-09 02:16:22 -0300

    alterações cadastros
    alteração cliente para cadastro jaqueta e criação cadastro estoque

--- application/views/Cliente/Cadastrar.php
+++ application/views/Cliente/Cadastrar.php
@@ -63,26 +63,40 @@
 <div class="container posicaopainel">
 	 	<div class="panel panel-default">
         		<div class="panel-heading">
-        			<h3><span class="glyphicon glyphicon-thumbs-up"></span> Gerenciar Cliente</h3>
+        			<h3></span> Cadastrar Jaquetas</h3>
         		</div>
         		<div class="panel-body">
         			<div class="container">
         				<form class="form" role="form" method="post" action="gerenciarClienteModel.php">
         				  	<label for="codigoCliente" class="sr-only">Código</label>
         				  	<input type="text" id="idCliente" name="idCliente" value="" class="form-control codigo" readonly="readonly"> 
-        		
-        				   	<label for="nome">Nome:</label>
+        					<br>
+        				   	<label for="nome">Jaqueta:</label>
         				   	<div class="input-group textos">
-								<input type="text" id="nome" name="nome" value= "" class="form-control" placeholder="Nome Cliente" required autofocus >
-									<span class="glyphicon glyphicon-search input-group-addon btn" onclick="fBusca()"></span>
+								<input type="Jaqueta" id="Jaqueta" style="width:260px"; name="nome" value= "" class="form-control" placeholder="Nome Jaqueta" required autofocus >
 							</div>
-        				   <label for="telefone">Telefone:</label>
-        				   <div class="input-group telefone">
-        				   	<span class="glyphicon glyphicon-earphone input-group-addon"> </span>	
-        				   	<input type="tel" id="telefone" name="telefone" value= "" class="form-control" placeholder="Telefone" required  >
-        				   <input type= "hidden" id="op" name="op" value="">
+							<br>
+        				   <label for="textos">Tecido:</label>
+        				   <div class="input-group textos">
+        				   	<input type="text" id="Tecidofone" name="Tecido" value= "" class="form-control" placeholder="Tecido" required  >
         				   </div>
         				   <br>
+        				   <label for="textos">Valor:</label>
+        				   <div class="input-group telefone">
+        				   	<input type="text" id="marca" name="valor" value= "" class="form-control" placeholder="Valor" required  >
+        				   </div>
+        				   <br>
+        				   <label for="textos">Descrição:</label>
+        				   <div class="input-group telefone">
+        				   	<input type="text" id="descricao" name="descricao" value= "" class="form-control" placeholder="Descrição" required  >
+        				   </div>
+        				   <br>
+        				    <label for="textos">Marca:</label>
+        				   <div class="input-group telefone">
+        				   	<input type="text" id="marca" name="marca" value= "" class="form-control" placeholder="Marca" required  >
+        				   </div>
+        				   <br>
+        				   
         				   <button onclick="document.getElementById('op').value='insert';" type="submit" class="btn btn-lg btn-primary" >Cadastrar</button>
         				   <button onclick="document.getElementById('op').value='update';" type="submit" class="btn btn-lg btn-primary" >Alterar</button>
         				   <button id="btnlimpar" type="submit" class="btn btn-lg btn-warning">Limpar</button>
@@ -95,13 +109,19 @@
         					<thead>
         						<tr>
         							<th># </th>
-        							<th>Nome</th>
-        							<th>Telefone</th>
+        							<th>Jaqueta/th>
+        							<th>tecido</th>
+        							<th>Valor</th>
+        							<th>Descição</th>
+        							<th>Marca</th>
         						</tr>
         					</thead>
         					 <tbody>
 								<tr>
 									<td><a href="gerenciarcliente.php?id="></a></td>
+									<td></td>
+									<td></td>
+									<td></td>
 									<td></td>
 									<td></td>
 								</tr>
