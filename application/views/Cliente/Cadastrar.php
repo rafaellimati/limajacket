051c1c0c8dbb3ceb3ccef9eb3504f699b4aa7bd9
rafaellimati <rafaelprofissionalti@gmail.com>
2015-08-08 23:08:46 -0300

    Mudança de css e adição jquerys
    adição de html e mudanças em css

--- application/views/Cliente/Cadastrar.php
+++ application/views/Cliente/Cadastrar.php
@@ -1,3 +1,65 @@
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>gerenciarcliente</title>
+		<meta name="author" content="Rafael" />
+		<!-- Date: 2014-11-20 -->
+		<link type="text/css" href="<?php echo base_url ("static/css/bootstrap.css")?>" rel="stylesheet" media="screen">
+		<link type="text/css" href="<?php echo base_url ("static/css/Estilos.css")?>" rel="stylesheet" media="screen">
+		
+		<script type="text/javaScript" src="<?php echo base_url("static/js/jquery-1.10.1.mini.js")?>"</script>
+		<script type="text/javaScript" src="<?php echo base_url("static/Js/jquery.maskedinput.min.js")?>"></script>
+		
+		<script type="text/javascript">
+		function  fBusca(){
+			busca=document.getElementById('nome').value;
+			url="gerenciarcliente.php?nomeCliente="+busca;
+			window.location=url;
+			}
+						/*jQuery(document).ready(function() {
+            //Inicio Mascara Telefone
+            jQuery('input[type=tel]').mask("(99) 9999-9999?9").ready(function(event) {
+                var target, phone, element;
+                target = (event.currentTarget) ? event.currentTarget : event.srcElement;
+                phone = target.value.replace(/\D/g, '');
+                element = $(target);
+                element.unmask();
+                if(phone.length > 10) {
+                    element.mask("(99) 99999-999?9");
+                } else {
+                    element.mask("(99) 9999-9999?9");
+                }
+            });
+            //Fim Mascara Telefone
+            //Inicio Mascara RG
+            //jQuery("#rg").mask("99.999.999-*");
+            //Fim Mascara CPF
+            //Inicio Mascara CPF
+            //jQuery("#cpf").mask("999.999.999-99");
+            //Fim Mascara CPF
+        });
+        (jQuery);/*/
+       
+       $(document).ready(function(){
+       	  $('#btnlimpar').click(function(){
+       	  	$('div').each(function(i){
+       	  		alert(i);
+       	  		$(this).css('background','red');
+       	  	});
+       	  });
+       });
+       
+       
+       $('#nome').click(function(){
+        $this.css('background','#c2e9f1');
+       })
+       
+       $('#btnlimpar').disable='True';
+       
+       
+		</script>
+		
+	
+	</head>
 <div class="container posicaopainel">
 	 	<div class="panel panel-default">
         		<div class="panel-heading">
@@ -7,17 +69,17 @@
         			<div class="container">
         				<form class="form" role="form" method="post" action="gerenciarClienteModel.php">
         				  	<label for="codigoCliente" class="sr-only">Código</label>
-        				  	<input type="text" id="idCliente" name="idCliente" value="<?=$idCliente?>" class="form-control codigo" readonly="readonly"> 
+        				  	<input type="text" id="idCliente" name="idCliente" value="" class="form-control codigo" readonly="readonly"> 
         		
         				   	<label for="nome">Nome:</label>
         				   	<div class="input-group textos">
-								<input type="text" id="nome" name="nome" value= "<?=$nome?>" class="form-control" placeholder="Nome Cliente" required autofocus >
+								<input type="text" id="nome" name="nome" value= "" class="form-control" placeholder="Nome Cliente" required autofocus >
 									<span class="glyphicon glyphicon-search input-group-addon btn" onclick="fBusca()"></span>
 							</div>
         				   <label for="telefone">Telefone:</label>
         				   <div class="input-group telefone">
         				   	<span class="glyphicon glyphicon-earphone input-group-addon"> </span>	
-        				   	<input type="tel" id="telefone" name="telefone" value= "<?=$telefone?>" class="form-control" placeholder="Telefone" required  >
+        				   	<input type="tel" id="telefone" name="telefone" value= "" class="form-control" placeholder="Telefone" required  >
         				   <input type= "hidden" id="op" name="op" value="">
         				   </div>
         				   <br>
