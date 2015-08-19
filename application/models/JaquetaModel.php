c3b39a6371a37cccd84e021721e6d7ba0a1ec03b
Bleno <blenobok@gmail.com>
2015-08-19 00:48:26 -0300

    Inner join das jaquetas e datatables das jaquetas

--- application/models/JaquetaModel.php
+++ application/models/JaquetaModel.php
@@ -17,12 +17,37 @@ class JaquetaModel extends CI_Model{
 	
 	public function getAllJaqueta(){
 
-		$this->db->from('TbJaqueta');
+/*		$this->db->from('TbJaqueta');
 
 		$this->db->order_by('idJaqueta');
 
 		//$this->db->where('ativo', 1);
 
+		return $this->db->get();*/
+
+
+
+		$this->db->select('idJaqueta ,jaqueta , tecido, valor,tbjaqueta.descricao as detalhes ,  marca, cor, tbstatus.descricao as statusDesc, tamanho');
+
+
+		$this->db->from('TbJaqueta');
+
+		$this->db->order_by('jaqueta');
+
+		$this->db->join('tbmarca', 'tbjaqueta.idMarca = tbmarca.idMarca', 'inner');
+		$this->db->join('tbcor', 'tbjaqueta.idCor = tbcor.idCor ', 'inner');
+		$this->db->join('tbstatus', 'tbjaqueta.idStatus = tbstatus.idStatus', 'inner');
+		$this->db->join('tbtamanho', 'tbjaqueta.idTamanho = tbtamanho.idTamanho', 'inner');
+
 		return $this->db->get();
+
+/*//$this->db->join('tb_docente', 'tb_docente.id = ta_mapeamento.id_docente', 'inner');
+inner join tbmarca on(tbjaqueta.idMarca = tbmarca.idMarca)
+inner join tbcor on(tbjaqueta.idCor = tbcor.idCor )
+inner join tbstatus on(tbjaqueta.idStatus = tbstatus.idStatus)
+inner join tbtamanho on(tbjaqueta.idTamanho = tbtamanho.idTamanho)*/
+
+
+
 	}
 }
--- application/views/cadastrarJaqueta/Jaqueta.php
+++ application/views/cadastrarJaqueta/Jaqueta.php
@@ -87,7 +87,7 @@
         				<p> </p>
         				
         				<div class="table-responsive tabelas">
-        				<table class="table table-bordered table-hover">
+        				<table class="table table-bordered table-hover dataTables">
         					<thead>
         						<tr>
         							<th># </th>
@@ -110,11 +110,11 @@
 									<td><?php echo $row->jaqueta;?></td>
 									<td><?php echo $row->tecido;?></td>
 									<td><?php echo $row->valor;?></td>
-									<td><?php echo $row->descricao;?></td>
-									<td><?php echo $row->idMarca;?></td>
-									<td><?php echo $row->idCor;?></td>
-									<td><?php echo $row->idTamanho;?></td>
-									<td><?php echo $row->descricao;?></td>
+									<td><?php echo $row->detalhes;?></td>
+									<td><?php echo $row->marca;?></td>
+									<td><?php echo $row->cor;?></td>
+									<td><?php echo $row->tamanho;?></td>
+									<td><?php echo $row->statusDesc;?></td>
 									
 								</tr>
 						     <?php 
@@ -126,3 +126,29 @@
         		</div>
 	 	</div>
 	 </div>
+     <script type="text/javascript">
+
+    $(document).ready(function() {
+        $('.dataTables').DataTable({
+                "responsive": true,
+                "oLanguage": {
+                        "oPaginate": { "sFirst": "<<", "sLast": ">>", "sNext": ">", "sPrevious": "<" },
+                        "sEmptyTable": 'Não foram encontrados registros. Tabela Vazia!',
+                        "sInfo": "<span>Exibindo de <b>_START_</b> até <b>_END_</b> de <b>_TOTAL_</b> registros encontrados.</span>",
+                        "sInfoEmpty": " ",
+                        "sInfoFiltered": "",
+                        "sInfoThousands": ".",
+                        "sLengthMenu": "Exibir _MENU_ registros",
+                        "sLoadingRecords": "<center>Carregando...</center>",
+                        "sProcessing": '<b>Processando...</b>', //"Processando...",
+                        "sSearch": "Pesquisa:",
+                        "sZeroRecords": "<center>Não foram encontrados registros.</center>"
+                },
+                "sPaginationType": "full_numbers",
+                "bFilter": true,
+                "bProcessing": true,
+                "bServerSide": false
+        });
+    });
+
+</script>
