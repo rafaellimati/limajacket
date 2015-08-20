7028304a2da48fb788c0357947bc69c46d1d217f
rafaellimati <rafaelprofissionalti@gmail.com>
2015-08-19 21:46:35 -0300

    pequenas alterações
    pequenas

--- application/views/usuario/cadastrar.php
+++ application/views/usuario/cadastrar.php
@@ -28,7 +28,7 @@
         				<p> </p>
         				
         				<div class="table-responsive tabelas">
-        				<table class="table table-bordered table-hover">
+        				<table class="table table-bordered table-hover dataTables">
         					<thead>
         						<tr>
         							<th># </th>
@@ -51,5 +51,31 @@
         				</div>
         		    </div>
         		</div>
+        		<script type="text/javascript">
+        $(document).ready(function() {
+        $('.dataTables').DataTable({
+                "responsive": true,
+                "oLanguage": {
+                        "oPaginate": { "sFirst": "<<", "sLast": ">>", "sNext": ">", "sPrevious": "<" },
+                        "sEmptyTable": 'Não foram encontrados registros. Tabela Vazia!',
+                        "sInfo": "<span>Exibindo de <b>_START_</b> até <b>_END_</b> de <b>_TOTAL_</b> registros encontrados.</span>",
+                        "sInfoEmpty": " ",
+                        "sInfoFiltered": "",
+                        "sInfoThousands": ".",
+                        "sLengthMenu": "Exibir _MENU_ registros",
+                        "sLoadingRecords": "<center>Carregando...</center>",
+                        "sProcessing": '<b>Processando...</b>', //"Processando...",
+                        "sSearch": "Pesquisa:",
+                        "sZeroRecords": "<center>Não foram encontrados registros.</center>"
+                },
+                "sPaginationType": "full_numbers",
+                "bFilter": true,
+                "bProcessing": true,
+                "bServerSide": false
+        });
+    });
+
+</script>
 	 	</div>
 	 </div>
+	 
