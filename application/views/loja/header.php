689c3cd551e4300eeddaa18fe516386a4ac046f8
Bleno <blenobok@gmail.com>
2015-08-23 01:21:51 -0300

    Adicionando o layout fatiado

---
+++ application/views/loja/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<title>Lima Jacket</title>
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" href="<?php echo base_url("static/css/style.css"); ?>" type="text/css" media="all" />
+<!--[if lte IE 6]><link rel="stylesheet" href="<?php echo base_url("static/css/ie6.css"); ?>" type="text/css" media="all" /><![endif]-->
+<script src="<?php echo base_url("static/js/jquery-1.4.1.min.js"); ?>" type="text/javascript"></script>
+<script src="<?php echo base_url("static/js/jquery.jcarousel.pack.js"); ?>" type="text/javascript"></script>
+<script src="<?php echo base_url("static/js/jquery-func.js"); ?>" type="text/javascript"></script>
+</head>
+<body>
+<!-- Shell -->
+<div class="shell">
+  <!-- Header -->
+  <div id="header">
+    <h1 id="logo"><a href="#">Lima Jacket</a></h1>
+    <!-- Cart -->
+    <div id="cart"> <a href="#" class="cart-link">Carrinho</a>
+      <div class="cl">&nbsp;</div>
+      <span>Produto: <strong>4</strong></span> &nbsp;&nbsp; <span>Custo: <strong>$250.99</strong></span> </div>
+    <!-- End Cart -->
+    <!-- Navigation -->
+    <div id="navigation">
+      <ul>
+        <li><a href="#" class="active">Home</a></li>
+        <li><a href="#">Compras</a></li>
+        <li><a href="#">Pedidos</a></li>
+        <li><a href="#">Quem somos</a></li>
+        <li><a href="#">Contato</a></li>
+      </ul>
+    </div>
+    <!-- End Navigation -->
+  </div>
+  <!-- End Header -->
